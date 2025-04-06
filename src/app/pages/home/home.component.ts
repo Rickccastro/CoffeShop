1960bdf0146e74e6a805c106b575b00228ab8209
ricardo <ricardocastroparmezan@gmail.com>
2025-04-06 15:22:25 -0300

    Feat: Iniciando segunda parte da pagina

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
-import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { ButtonComponent } from "../../components/button/button.component";
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, NavbarComponent, ButtonComponent],
+  imports: [HeaderComponent, ButtonComponent,CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
