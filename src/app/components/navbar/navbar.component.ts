4fbb1349b6f6cd77ac74a79bed7e89e3a7d7ed89
ricardo <ricardocastroparmezan@gmail.com>
2025-04-06 01:25:15 -0300

    Feat: Finalizando navbar e Header

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { ButtonComponent } from '../button/button.component';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule,ButtonComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
