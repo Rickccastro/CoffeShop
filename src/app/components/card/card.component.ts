ba1fb9516d5a83871ec2e4f715ac3fb9bf927505
ricardo <ricardocastroparmezan@gmail.com>
2025-04-06 15:22:55 -0300

    Feat: Criando componente Card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import { ButtonComponent } from '../button/button.component';
+
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, ButtonComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
