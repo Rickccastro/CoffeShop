30368e555c8d221695de1491aad6f15ad75adcd5
ricardo <ricardocastroparmezan@gmail.com>
2025-04-06 19:08:06 -0300

    Feat: Criando componente de lista de cards

--- src/app/components/list-products/list-products.component.ts
+++ src/app/components/list-products/list-products.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-list-products',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './list-products.component.html',
   styleUrl: './list-products.component.css'
 })
 export class ListProductsComponent {
+  coffes = signal<{ name: string; price: number }[]>(this.getCoffes())
+  getCoffes(): Coffe[] {
 
+    return { 'Espresso',  2.5 }; // Example object
+  }
 }
