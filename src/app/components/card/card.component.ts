c3203039fe3d45150351ba3fd31090e541f3926a
ricardo <ricardocastroparmezan@gmail.com>
2025-04-06 19:07:46 -0300

    Refactor: Deixando componente card e button dinamicos

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
+import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { ButtonComponent } from '../button/button.component';
-
 
 @Component({
   selector: 'app-card',
   standalone: true,
   imports: [MatCardModule, ButtonComponent],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
 export class CardComponent {
+  @Input() imageSrc: string = '';
+  @Input() imageAlt: string = '';
+  @Input() title: string = '';
+  @Input() subtitle: string = '';
+  @Input() secondSubtitle: string = '';
 
+  @Input() showButton: boolean = true;
+  @Input() buttonLabel: string = 'Order Now';
 }
