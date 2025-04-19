1a4c0e82bd58e1f2be2ad4a9cba993142c87d3f3
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-04-18 21:46:55 -0300

    Feat: responsividade no componente header e navbar

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent,NavbarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
-import { ButtonComponent } from '../button/button.component';
-
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule, MatIconRegistry} from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule,ButtonComponent],
+  imports: [MatToolbarModule, MatButtonModule,MatMenuModule,MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent{
   buttonIn: string = 'Sign In';
   buttonOut: string = 'Sign Out';
+
+  constructor(
+    private iconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer
+  ) {
+    this.iconRegistry.addSvgIcon(
+      'menu-icon-registred',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/Menu.svg')
+    );
+  } 
 }
