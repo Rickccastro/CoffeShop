2622dfc19c90e1848af1cfdd85a648ab88590424
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-04-23 17:30:17 -0300

    Refactor: utilizado mat-menu em cascata

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,28 +1,35 @@
-import { Component, inject, OnInit} from '@angular/core';
+import { Component} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatIconModule, MatIconRegistry} from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule,MatMenuModule,MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule,MatMenuModule,MatIconModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent{
   buttonIn: string = 'Sign In';
   buttonOut: string = 'Sign Out';
-
   menuItems = [
     { label: 'Home', link: '#' },
-    { label: 'Produtos', link: '#' },
+    {
+      label: 'Products',
+      submenu: [
+        { label: 'Cafés', link: '#' },
+        { label: 'Acompanhamentos', link: '#' },
+      ]
+    },
     { label: 'About us', link: '#' },
     { label: 'Contact us', link: '#' },
-    { label: 'Account', link: '#' },
+    { label: 'Account', link: '#' }
   ];
+  
 
   constructor(
     private iconRegistry: MatIconRegistry,
@@ -33,4 +40,8 @@ export class NavbarComponent{
       this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/Menu.svg')
     );
   } 
+  getSubmenu(label: string) {
+    const item = this.menuItems.find(i => i.label === label);
+    return item?.submenu ?? [];
+  }
 }
