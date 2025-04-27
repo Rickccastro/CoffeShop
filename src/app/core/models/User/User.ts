939c4cfb661b9a8138cf0ca2f444b4eed579a74e
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-04-27 16:51:35 -0300

    Feat: criando model e serviço do User

---
+++ src/app/core/models/User/User.ts
@@ -0,0 +1,10 @@
+export interface User {
+    id?: string;
+    Nome?: string;
+    Email?: string;
+    Senha?: string;
+    imageSrc?: string;
+    imageAlt?: string;
+}
+  
+  
---
+++ src/app/shared/services/user.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { User } from '../../core/models/User/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  httpClient = inject(HttpClient);
+  produtos: any[] = []
+
+  cadastroEmailNotification(user: User){
+    return this.httpClient.post<User>('http://localhost:3000/users', user);
+ }
+}
