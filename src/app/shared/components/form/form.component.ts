7c544fcd51e883be42d1696a93695f583e0f6676
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-04-27 23:10:55 -0300

    Fix: correção na renderização do estilo do input

--- src/app/shared/components/form/form.component.ts
+++ src/app/shared/components/form/form.component.ts
@@ -1,33 +1,57 @@
-import { Component, EventEmitter, inject, input, Output} from '@angular/core';
-import { FormGroup, FormBuilder, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { Component, EventEmitter, input, Output } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { ButtonComponent } from "../button/button.component";
+import { ButtonComponent } from '../button/button.component';
+import { User } from '../../../core/models/User/User';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [ReactiveFormsModule, FormsModule, MatFormFieldModule, MatInputModule, ButtonComponent],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ButtonComponent,
+  ],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.css'
+  styleUrl: './form.component.css',
 })
 export class FormComponent {
-  // @Output() send = new EventEmitter<User>();
-  // product = input<User | null >(null);
-  form! : FormGroup;
+  @Output() send = new EventEmitter<User>();
+  user = input<User | null>(null);
+  form!: FormGroup;
 
-  // ngOnInit(): void {
-  //   this.form = new FormGroup(
-  //     {
-  //         title : new FormControl<string>(this.product()?.title ?? '',{
-  //         nonNullable : true,
-  //         validators: Validators.required}),
-  //     }
-  //   ); 
-  // }
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      cpf: new FormControl<string>(this.user()?.Cpf ?? '', {
+        nonNullable: true,
+        validators: Validators.required,
+      }),
+      nome: new FormControl<string>(this.user()?.Nome ?? '', {
+        nonNullable: true,
+        validators: Validators.required,
+      }),
 
-  onSubmit(){
-    // const userData = this.form.value as User;
-    // this.send.emit(product)
+      email: new FormControl<string>(this.user()?.Email ?? '', {
+        nonNullable: true,
+        validators: Validators.required,
+      }),
+
+      senha: new FormControl<string>(this.user()?.Senha ?? '', {
+        nonNullable: true,
+        validators: Validators.required,
+      }),
+    });
+  }
+
+  onSubmit() {
+    const user = this.form.value as User;
+    this.send.emit(user);
   }
 }
