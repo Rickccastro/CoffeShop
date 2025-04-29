5d2906169a11c698164900d30ddf93df8603a630
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-04-28 23:46:02 -0300

    Feat: Criação do metodo de cadastro de usuarios

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -3,13 +3,18 @@ import { inject, Injectable } from '@angular/core';
 import { User } from '../../core/models/User/User';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
   httpClient = inject(HttpClient);
-  produtos: any[] = []
+  produtos: any[] = [];
 
-  cadastroEmailNotification(user: User){
+  cadastroEmailNotification(user: User) {
     return this.httpClient.post<User>('http://localhost:3000/users', user);
- }
+  }
+
+  cadastroUser(user: User) {
+    return console.log("cadastroUser")
+    // return this.httpClient.post<User>('http://localhost:3000/users', user);
+  }
 }
