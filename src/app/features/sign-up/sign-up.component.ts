5724a48fcf40ed25a8b817350eb6dd292d7d65bd
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-04-28 23:44:10 -0300

    Feat: adicionando splash's

--- src/app/features/sign-up/sign-up.component.ts
+++ src/app/features/sign-up/sign-up.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormComponent } from '../../shared/components/form/form.component';
 import { NotificationFormComponent } from "../../shared/components/notification-form/notification-form.component";
+import { UserService } from '../../shared/services/user.service';
+import { Router } from '@angular/router';
+import { User } from '../../core/models/User/User';
 
 @Component({
   selector: 'app-sign-up',
@@ -10,5 +13,15 @@ import { NotificationFormComponent } from "../../shared/components/notification-
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
-
+  userService = inject(UserService);
+  router= inject(Router) 
+      
+  onSubmit(user: User) 
+  {
+     this.userService.cadastroUser(user);
+    // .subscribe((data)=>{
+    //   alert(`Usuario ${data.Nome} cadastrado com sucesso!`);
+    //   this.router.navigateByUrl('/');
+    // });
+  }    
 }
