0e8bca9d4355c52c35408ce737dab2a324dc9942
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-05-01 13:58:21 -0300

    Fix: implementação do subscribe para a requisição http

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,7 +1,8 @@
 import { Component, inject } from '@angular/core';
-import { NotificationFormComponent } from "../../../shared/components/notification-form/notification-form.component";
 import { User } from '../../models/User/User';
 import { UserService } from '../../../shared/services/user.service';
+import { Router } from '@angular/router';
+import { NotificationFormComponent } from '../../../shared/components/forms/notification-form/notification-form.component';
 
 @Component({
   selector: 'app-footer',
@@ -12,9 +13,12 @@ import { UserService } from '../../../shared/services/user.service';
 })
 export class FooterComponent {
   userService = inject(UserService);
+  router= inject(Router) 
 
   onSubmit(user: User) 
   {
-    this.userService.cadastroEmailNotification(user);
-  }    
+    this.userService.cadastroEmailNotification(user).subscribe((data)=>{
+      alert(`Email ${data.nome} cadastrado com sucesso!`);
+    });
+  }   
 }
