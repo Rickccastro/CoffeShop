66660093a8e851c2062a71680a199076f01386bf
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-05-01 13:57:38 -0300

    Feat: criação pagina de login

--- src/app/features/sign-in/sign-in.component.ts
+++ src/app/features/sign-in/sign-in.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../../core/models/User/User';
+import { UserService } from '../../shared/services/user.service';
+import { FormComponent } from '../../shared/components/forms/register-form/register-form.component';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [FormComponent],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
-
+  userService = inject(UserService);
+  router= inject(Router) 
+      
+  onSubmit(user: User) 
+  {
+     this.userService.loginUser(user)
+    .subscribe((data)=>{
+      alert(`Usuario ${data.nome} Login com sucesso!`);
+      this.router.navigateByUrl('/');
+    });
+  } 
 }
