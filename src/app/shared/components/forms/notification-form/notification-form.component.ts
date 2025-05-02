7cd3f43e90fc9cb91bbd62b002739b903df8ba4d
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-05-01 23:49:42 -0300

    Refactor: Utilizando component input form para campos no formularios

---
+++ src/app/shared/components/forms/notification-form/notification-form.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ButtonComponent } from '../../button/button.component';
+import { User } from '../../../../core/models/User/User';
+import { InputComponent } from '../input-form/input-form.component';
+
+@Component({
+  selector: 'app-notification-form',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    InputComponent,
+    MatInputModule,
+    ButtonComponent,
+  ],
+  templateUrl: './notification-form.component.html',
+  styleUrl: './notification-form.component.css',
+})
+export class NotificationFormComponent {
+  @Output() send = new EventEmitter<User>();
+  @Input() user: User | null = null;
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  onSubmit() {
+    const user = { email: this.emailFormControl.value } as User;
+    this.send.emit(user);
+  }
+}
---
+++ src/app/shared/components/forms/register-form/register-form.component.ts
@@ -0,0 +1,63 @@
+import { Component, EventEmitter, input, Output } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ButtonComponent } from '../../button/button.component';
+import { User } from '../../../../core/models/User/User';
+import { InputComponent } from '../input-form/input-form.component';
+
+@Component({
+  selector: 'app-register-form',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ButtonComponent,
+    InputComponent
+  ],
+  templateUrl: './register-form.component.html',
+  styleUrl: './register-form.component.css',
+})
+export class FormComponent {
+  @Output() send = new EventEmitter<User>();
+  user = input<User | null>(null);
+  form!: FormGroup;
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      cpf: new FormControl<string>(this.user()?.cpf ?? '', {
+        nonNullable: true,
+        validators: [Validators.required,]
+      }),
+      nome: new FormControl<string>(this.user()?.nome ?? '', {
+        nonNullable: true,
+        validators: Validators.required,
+      }),
+
+      email: new FormControl<string>(this.user()?.email ?? '', {
+        nonNullable: true,
+        validators: [Validators.required,Validators.email]
+      }),
+
+      senha: new FormControl<string>(this.user()?.senha ?? '', {
+        nonNullable: true,
+        validators: [Validators.required, Validators.pattern("^[0-9]+$")]
+      }),
+    });
+  }
+
+  onSubmit() {
+    const user = this.form.value as User;
+    this.send.emit(user);
+  }
+
+  getControl(controlName: string): FormControl {
+    return this.form.get(controlName) as FormControl;
+  }  
+}
