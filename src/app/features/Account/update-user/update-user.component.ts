7c1940ae675cb7c660294aa9943b6fc6af9af217
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-05-03 00:31:46 -0300

    Feat: Criação da pagina update user

---
+++ src/app/features/Account/update-user/update-user.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject } from '@angular/core';
+import { UpdateFormComponent } from '../../../shared/components/forms/update-form/update-form.component';
+import { Router } from '@angular/router';
+import { User } from '../../../core/models/User/User';
+import { UserService } from '../../../shared/services/user.service';
+
+@Component({
+  selector: 'app-update-user',
+  standalone: true,
+  imports: [UpdateFormComponent],
+  templateUrl: './update-user.component.html',
+  styleUrl: './update-user.component.css'
+})
+export class UpdateUserComponent {
+  userService = inject(UserService);
+  router= inject(Router) 
+      
+  onSubmit(user: User) 
+  {
+     this.userService.atualizarUser(user)
+    .subscribe((data)=>{
+      alert(`Usuario ${data.nome} atualizado com sucesso!`);
+      this.router.navigateByUrl('/');
+    });
+  }  
+}
