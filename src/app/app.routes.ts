e2a2c085cb4bcc77d28839da2684019ce65f8361
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-05-03 00:36:32 -0300

    Feat: Implementando rotas das paginas de sign in sing up e update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,9 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { coffeeResolver } from './resolvers/coffe.resolver';
 import { feedbackResolver } from './resolvers/feedbacks.resolver';
-import { SignInComponent } from './features/sign-in/sign-in.component';
-import { SignUpComponent } from './features/sign-up/sign-up.component';
+import { SignInComponent } from './features/Account/sign-in/sign-in.component';
+import { UpdateUserComponent } from './features/Account/update-user/update-user.component';
+import { SignUpComponent } from './features/Account/sign-up/sign-up.component';
 
 
 export const routes: Routes = [
@@ -18,5 +19,13 @@ export const routes: Routes = [
  {
    path:'sign-up',
    component:SignUpComponent,
- }, 
+ },
+ {
+  path:'sign-in',
+  component:SignInComponent,
+}, 
+ {
+  path:'update-user',
+  component:UpdateUserComponent,
+}, 
 ];
