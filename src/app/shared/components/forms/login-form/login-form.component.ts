597173a0257c57f72075d79f59d7dea33984d0d3
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-05-03 00:32:32 -0300

    Feat: Criação do login form

---
+++ src/app/shared/components/forms/login-form/login-form.component.ts
@@ -0,0 +1,49 @@
+import { Component, EventEmitter, input, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ButtonComponent } from '../../button/button.component';
+import { InputComponent } from '../input-form/input-form.component';
+import { User } from '../../../../core/models/User/User';
+
+@Component({
+  selector: 'app-login-form',
+  standalone: true,
+   imports: [
+       ReactiveFormsModule,
+       MatFormFieldModule,
+       MatInputModule,
+       ButtonComponent,
+       InputComponent
+     ],
+  templateUrl: './login-form.component.html',
+  styleUrl: './login-form.component.css'
+})
+export class LoginFormComponent {
+ @Output() send = new EventEmitter<User>();
+    user = input<User | null>(null);
+    form!: FormGroup;
+  
+    ngOnInit(): void {
+      this.form = new FormGroup({  
+        email: new FormControl<string>(this.user()?.email ?? '', {
+          nonNullable: true,
+          validators: [Validators.required,Validators.email]
+        }),
+  
+        senha: new FormControl<string>(this.user()?.senha ?? '', {
+          nonNullable: true,
+          validators: [Validators.required, Validators.pattern("^[0-9]+$")]
+        }),
+      });
+    }
+  
+    onSubmit() {
+      const user = this.form.value as User;
+      this.send.emit(user);
+    }
+  
+    getControl(controlName: string): FormControl {
+      return this.form.get(controlName) as FormControl;
+    } 
+}
