a9f8fbbf7a47dab8066700a54a2f6eb6170f1f01
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-05-03 22:18:45 -0300

    Feat: Adicionando rota payment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { feedbackResolver } from './resolvers/feedbacks.resolver';
 import { SignInComponent } from './features/Account/sign-in/sign-in.component';
 import { UpdateUserComponent } from './features/Account/update-user/update-user.component';
 import { SignUpComponent } from './features/Account/sign-up/sign-up.component';
+import { PaymentComponent } from './features/payment/payment/payment.component';
 
 
 export const routes: Routes = [
@@ -27,5 +28,9 @@ export const routes: Routes = [
  {
   path:'update-user',
   component:UpdateUserComponent,
+}, 
+{
+  path:'payment',
+  component:PaymentComponent,
 }, 
 ];
