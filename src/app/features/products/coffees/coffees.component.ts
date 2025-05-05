14810614c7d8af0c96b34380b181a0ba9032b293
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-05-04 22:03:42 -0300

    Feat: Implementando cardapio de coffees na pagina

--- src/app/features/products/coffees/coffees.component.ts
+++ src/app/features/products/coffees/coffees.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, signal } from '@angular/core';
+import { ListProductsComponent } from "../../../shared/components/list-products/list-products.component";
+import { CardDisplay } from '../../../core/models/CardDisplay';
+import { Coffe } from '../../../core/models/Coffe';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-coffees',
   standalone: true,
-  imports: [],
+  imports: [ListProductsComponent],
   templateUrl: './coffees.component.html',
   styleUrl: './coffees.component.css'
 })
 export class CoffeesComponent {
+    coffees = signal<Coffe[]>([]); 
 
+    private route = inject(ActivatedRoute);
+      ngOnInit() {
+        this.route.data.subscribe((data) => {
+          this.coffees.set(data['coffe']);
+        });
+      }
 }
