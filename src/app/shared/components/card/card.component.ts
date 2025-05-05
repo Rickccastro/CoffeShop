3bf4a16cec07a78908dc3a38a05938baa8a535d1
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-05-04 22:21:29 -0300

    Fix: corrigindo estilos

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -12,7 +12,7 @@ import { CardDisplay } from '../../../core/models/CardDisplay';
 })
 export class CardComponent {
   @Input() data: Partial<CardDisplay> = {}; 
-  @Input() cardOption?: 'feedback' |'small' | 'default' = 'default';
+  @Input() cardOption?: 'feedback' |'small' |'grid'|'default' = 'default';
   @Input() showButton!: boolean;
   @Input() buttonText!: string;
 }
