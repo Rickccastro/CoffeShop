83287bfd40cc17625a7e051f713513d01e5dd1dc
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-05-06 00:27:45 -0300

    Feat: Adicionando form dialog para adicionar quantidade de produtos ao market

---
+++ src/app/shared/components/dialog/form-dialog/form-dialog.component.ts
@@ -0,0 +1,33 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { CardComponent } from '../../card/card.component';
+import { CardDisplay } from '../../../../core/models/CardDisplay';
+
+@Component({
+  selector: 'app-form-dialog',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule, CardComponent],
+  templateUrl: './form-dialog.component.html',
+  styleUrl: './form-dialog.component.css'
+})
+export class FormDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<FormDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { cardData: Partial<CardDisplay>; cardOption: string; buttonText: string }
+  ) {}
+
+  close(): void {
+    this.dialogRef.close();
+  }
+
+  confirmPayment(): void {
+    console.log('Pagamento confirmado para', this.data.cardData.title);
+    this.dialogRef.close(true);
+  }
+
+  handleCardButtonClick(option: string): void {
+    console.log('Botão clicado com opção:', option);
+  }
+}
