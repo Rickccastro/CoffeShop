bcc36940d77c37a3f2bba6cd0dee13b333ed8cca
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-05-14 20:01:47 -0300

    Feat: adicionando metodo para ao clicarmos no card da home page ele redirecionar para a pagina de coffees

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,7 +1,10 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { ButtonComponent } from '../button/button.component';
 import { CardDisplay } from '../../../core/models/CardDisplay';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-card',
@@ -11,15 +14,26 @@ import { CardDisplay } from '../../../core/models/CardDisplay';
   styleUrl: './card.component.css',
 })
 export class CardComponent {
-  @Input() cardData: Partial<CardDisplay> = {}; 
-  @Input() cardOption?: 'feedback' |'small' |'grid'| 'market' |'default' = 'default';
+  @Input() cardData: Partial<CardDisplay> = {};
+  @Input() cardOption?: 'feedback' | 'small' | 'grid' | 'market' | 'default' =
+    'default';
   @Input() showButton!: boolean;
   @Input() buttonText!: string;
-  @Output() buttonClick = new EventEmitter<Partial<CardDisplay>>(); 
+  @Output() buttonClick = new EventEmitter<Partial<CardDisplay>>();
+  dialog = inject(MatDialog);
+  router = inject(Router);
+  viewportScroller = inject(ViewportScroller);
+
   quantity: number = 1;
 
   handleButtonClick() {
-    this.buttonClick.emit(this.cardData);
+    if (this.cardOption === 'grid') {
+      this.buttonClick.emit(this.cardData);
+      return;
+    }
+    this.router.navigate(['/coffees']).then(() => {
+      this.viewportScroller.scrollToPosition([0, 0]);
+    });
   }
 
   increment() {
