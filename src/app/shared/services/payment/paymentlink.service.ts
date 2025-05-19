c73f5641f6ebf5d42c25aa4001da7f2a464f2278
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-05-18 22:45:13 -0300

    feat: criacao do payment link service

---
+++ src/app/shared/services/payment/paymentlink.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { PaymentItemDto } from '../../../core/models/PaymentItemDto';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PaymentLinkService {
+  httpClient = inject(HttpClient);
+
+  //  getPrices(listProdutos :Partial<CardDisplay>[]){
+  //    return this.httpClient.post<any>('/api/Prices', listProdutos);
+  //  }
+
+  getPaymentLink(listProdutos: PaymentItemDto[]) {
+    const payload = { items: listProdutos };
+
+    this.httpClient
+      .post<any>('https://localhost:7087/PaymentLink', payload)
+      .subscribe({
+        next: (response) => {
+          if (response && response.url) {
+            window.open(response.url, '_blank');
+          } else {
+            console.error('URL não encontrada na resposta:', response);
+          }
+        },
+        error: (err) => {
+          console.error('Erro ao gerar o link de pagamento:', err);
+        },
+      });
+  }
+}
