ad7cbd7d58e15ab3c37682e11c314a377b837821
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-05-20 23:07:23 -0300

    Feat: Criando Diretiva p os Cards

---
+++ src/app/shared/components/card/card-directive.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appCardDirective]',
+  standalone: true
+})
+export class CardDirectiveDirective {
+
+  constructor() { }
+
+}
