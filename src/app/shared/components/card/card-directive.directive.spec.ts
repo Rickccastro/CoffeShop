a26a49392b7546add6b3b00ae71e2865ee0d4f52
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-05-24 21:36:16 -0300

    Refactor: removendo diretivas

--- src/app/shared/components/card/card-directive.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CardDirectiveDirective } from './card-directive.directive';
-
-describe('CardDirectiveDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CardDirectiveDirective();
-    expect(directive).toBeTruthy();
-  });
-});
