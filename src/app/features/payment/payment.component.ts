919ef9c1f2312ce8b41b65e8890c1da809f7a24a
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-05-30 00:42:09 -0300

    Feat: Criação do checkout

--- src/app/features/payment/payment.component.ts
+++ src/app/features/payment/payment.component.ts
@@ -3,7 +3,7 @@ import { StripeEmbeddedCheckout, loadStripe, Stripe } from '@stripe/stripe-js';
 import { CheckoutSessionsService } from '../../shared/services/payment/checkout-sessions.service';
 import { CestaDialogComponent } from '../../shared/components/dialog/cesta-dialog/cesta-dialog.component';
 
-import { Environment } from '../../core/environments/enviroments'
+import { Environment } from '../../core/environments/environment';
 
 @Component({
   selector: 'app-payment',
@@ -15,34 +15,66 @@ import { Environment } from '../../core/environments/enviroments'
 export class PaymentComponent implements OnInit, AfterViewInit {
   stripe: Stripe | null = null;
   checkout: StripeEmbeddedCheckout | null = null;
+  clientSecret!: string;
   checkoutService = inject(CheckoutSessionsService);
-  cestaDialogComponent = inject(CestaDialogComponent);
-
-  constructor() {}
 
   async ngOnInit() {
-    this.stripe = await loadStripe(Environment.APP_KEY);
-  }
+    const paymentItems = this.getPaymentItems();
 
-  async ngAfterViewInit() {
-    if (!this.stripe) {
-      console.error('Stripe failed to load');
+    if (!paymentItems.length) {
+      console.error('Nenhum item de pagamento encontrado');
       return;
     }
 
-    // const fetchClientSecret = async (): Promise<string> => {
-    //   return new Promise<string>((resolve, reject) => {
-    //     this.cestaDialogComponent.finalizarPedido().subscribe({
-    //       next: (result: { clientSecret: string }) => resolve(result.clientSecret),
-    //       error: (err: any) => reject(err)
-    //     });
-    //   });
-    // };
+    await this.initializeCheckout(paymentItems);
+  }
 
-    // this.checkout = await this.stripe.initEmbeddedCheckout({
-    //   fetchClientSecret,
-    // });
+  private getPaymentItems() {
+    return this.checkoutService.getPaymentItems();
+  }
 
-    // this.checkout.mount('#checkout');
+  private async initializeCheckout(paymentItems: any[]) {
+    this.checkoutService.createCheckoutSessions(paymentItems).subscribe({
+      next: async (response) => {
+        this.handleCheckoutSessionResponse(response);
+      },
+      error: (err) => {
+        console.error('Erro ao criar sessão de checkout', err);
+      },
+    });
+  }
+
+  private async handleCheckoutSessionResponse(response: any) {
+    this.clientSecret = response.clientSecret;
+    this.checkoutService.setClientSecret(this.clientSecret);
+
+    await this.loadStripeInstance();
+    await this.mountCheckout();
+  }
+
+  private async loadStripeInstance() {
+    this.stripe = await loadStripe(Environment.APP_KEY);
+
+    if (!this.stripe) {
+      console.error('Erro ao carregar Stripe');
+      throw new Error('Stripe não carregado');
+    }
+  }
+
+  async ngAfterViewInit() {
+    this.mountCheckout();
+  }
+
+  async mountCheckout() {
+    if (!this.stripe) {
+      console.error('Stripe não carregado');
+      return;
+    }
+
+    this.checkout = await this.stripe.initEmbeddedCheckout({
+      clientSecret: this.clientSecret,
+    });
+
+    this.checkout.mount('#checkout');
   }
 }
