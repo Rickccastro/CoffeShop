3fc3ca739fda93b885caaab2adbe6d9043ca70cc
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-05-30 00:43:19 -0300

    Feat: criando metodos de service para o checkout

--- src/app/shared/services/payment/checkout-sessions.service.ts
+++ src/app/shared/services/payment/checkout-sessions.service.ts
@@ -4,15 +4,40 @@ import { PaymentItemDto } from '../../../core/models/PaymentItemDto';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CheckoutSessionsService {
+  private clientSecret!: string;
+  private paymentItems: { priceId: string; quantity: number }[] = [];
+
   httpClient = inject(HttpClient);
 
+  setPaymentItems(items: { priceId: string; quantity: number }[]) {
+    this.paymentItems = items;
+  }
 
-   createCheckoutSessions(listProdutos: PaymentItemDto[]): Observable<{ clientSecret: string }> {
+  getPaymentItems() {
+    return this.paymentItems;
+  }
+
+  clearPaymentItems() {
+    this.paymentItems = [];
+  }
+
+  setClientSecret(secret: string) {
+    this.clientSecret = secret;
+  }
+
+  getClientSecret() {
+    return this.clientSecret;
+  }
+  createCheckoutSessions(
+    listProdutos: PaymentItemDto[]
+  ): Observable<{ clientSecret: string }> {
     const payload = { items: listProdutos };
-    return this.httpClient.post<{ clientSecret: string }>
-    ('https://localhost:7087/CheckoutSession/create-checkout-session', payload);
+    return this.httpClient.post<{ clientSecret: string }>(
+      'https://localhost:7087/CheckoutSession/create-checkout-session',
+      payload
+    );
   }
 }
