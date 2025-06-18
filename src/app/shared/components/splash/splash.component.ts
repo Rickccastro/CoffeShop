b27c94d626647157a180d08b12ce9b15140a3620
Rickccastro <ricardoparmezancastro.dev@gmail.com>
2025-06-18 00:10:47 -0300

    Feat: criando componente splash

--- src/app/shared/components/splash/splash.component.ts
+++ src/app/shared/components/splash/splash.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-splash',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './splash.component.html',
   styleUrl: './splash.component.css'
 })
 export class SplashComponent {
-  @Input() position: 'esquerda' | 'direita' = 'esquerda';
+  @Input() position: 'esquerda' | 'direita' | 'baixo' = 'esquerda';
+
+  getSplashImage(): string {
+  const base = '../../../assets/coffes/coffe-splash';
+  console.log(`${base}-baixo.svg` );
+  return this.position === 'baixo' ? `${base}-baixo.svg` : `${base}.svg`;
+}
 }
