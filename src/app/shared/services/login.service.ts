31c8187dd53ed6bee76f1309e19ff9e59f53df69
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-07-26 03:53:41 -0300

    feat: adicionando endpoint para token do login

--- src/app/shared/services/login.service.ts
+++ src/app/shared/services/login.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { User } from '../../core/models/User/User';
-import { Login } from '../../core/models/Login';
+import { Login } from '../../core/models/Login/Login';
+import { Observable } from 'rxjs';
+import { LoginTokenValidatedRequest } from '../../core/models/Login/LoginTokenValidatedRequest';
 
 
 
@@ -11,9 +13,21 @@ import { Login } from '../../core/models/Login';
 export class LoginService {
     httpClient = inject(HttpClient);
   
-  loginUser(credentials: Login) { 
-    return this.httpClient.post<Login>('/api/User/login-user', credentials);
+  loginUser(credentials: Login) : Observable<Login>{ 
+    return this.httpClient.post<Login>('/api/Login/login-user', credentials);
   }
+
+   loginTokenValidated(credentials: Login, code: string): Observable<boolean> {
+    const payload: LoginTokenValidatedRequest = {
+      EmailNm: credentials.EmailNm,
+      UsrIntPassword: credentials.UsrIntPassword,
+      Code: code
+    };
+    
+    return this.httpClient.post<boolean>('/api/Login/validated-login-user', payload);
+  }
+
+
   logout() {
     localStorage.removeItem('token');
     localStorage.removeItem('user');
